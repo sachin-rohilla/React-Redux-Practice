62a99cb0fa8dbfd916ee7c65347182009faf9b51
sachin-rohilla <sachinrohilla23799@gmail.com>
2024-03-17 22:56:46 +0530

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,11 @@
 import React from "react";
 import { useState } from "react";
-import { createStore } from "redux";
-
-const initialState = {
-  name: "sachin",
-  age: 23,
-};
-
-function updateReducer(state = initialState, action) {
-  switch (action.type) {
-    case "increment":
-      return { ...state, age: state.age + 1 };
-    case "decrement":
-      return { ...state, age: state.age - 1 };
-    case "incrementBy":
-      return { ...state, age: state.age + action.payload };
-    default:
-      return state;
-  }
-}
-
-const store = createStore(updateReducer);
-
-store.subscribe(() => {
-  console.log(store.getState());
-});
-
-store.dispatch({ type: "increment" });
-store.dispatch({ type: "decrement" });
-store.dispatch({ type: "incrementBy", payload: 10 });
+import ReduxPractice from "./components/ReduxPractice/ReduxPractice";
 
 const App = () => {
   return (
     <div>
-      <h1>Redux</h1>
+      <ReduxPractice />
     </div>
   );
 };
---
+++ src/components/ReduxPractice/ReduxPractice.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { createStore } from "redux";
+
+const initialState = {
+  name: "sachin",
+  age: 23,
+};
+
+function updateReducer(state = initialState, action) {
+  switch (action.type) {
+    case "increment":
+      return { ...state, age: state.age + 1 };
+    case "decrement":
+      return { ...state, age: state.age - 1 };
+    case "incrementBy":
+      return { ...state, age: state.age + action.payload };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(updateReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
+store.dispatch({ type: "incrementBy", payload: 10 });
+const ReduxPractice = () => {
+  return <div>ReduxPractice</div>;
+};
+
+export default ReduxPractice;
